0028a3bd9993b548aabb814aed446f91eb57412a
Mahyar Abbaspour <m.abbaspour@pishro.computer>
2016-08-30 11:59:02 +0430

    Add profile view

--- csportal/csportal/urls.py
+++ csportal/csportal/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from portal import views
+from user.views import profile
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
-    url(r'^about/$', views.about)
-]
+    url(r'^about/$', views.about),
+    url(r'^users/(?P<username>[0-9]{9})/$', profile),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- csportal/user/views.py
+++ csportal/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+
+def profile(request, username=None):
+    context = {}
+    student = models.Student.objects.get(user__username=username)
+    context['student'] = student
+    return render(request, 'user/profile.html', context)
